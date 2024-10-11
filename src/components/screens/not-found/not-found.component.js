b88e601224e710edd7b89b42c86ddd583ae7a1c6
Andrei Kovalenko <kovalenkoandrej485@gmail.com>
2024-10-11 14:10:48 +0300

    complite routing

--- src/components/screens/not-found/not-found.component.js
+++ src/components/screens/not-found/not-found.component.js
@@ -1,4 +1,10 @@
-export class NotFound {
+import { BaseScreen } from '@/core/component/base-screen.component'
+
+export class NotFound extends BaseScreen {
+	constructor() {
+		super({ title: 'Not Found' })
+	}
+
 	render() {
 		return '<p>Not found<p>'
 	}
